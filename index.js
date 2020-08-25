6bab9eed9c71c8f271883621ecd2857168ea3b7b
Ryan Harman <ryan.harman63@gmail.com>
2020-08-25 17:08:01 +0100

    API + Animation
    Updated the books to use the Google Books API and remade the onload animation with JavaScript

--- index.js
+++ index.js
@@ -12,3 +12,69 @@ $(document).ready(function(){
 });
 
 
+// Provides animation when items come into view for the site
+const animateHTML = function() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('fade-in-element');
+        element.classList.remove('hidden');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+};
+
+
+// For collecting book image, title and link using Google Books API
+function getBooks() {
+  const bookIds = getBookIds();
+  console.log(bookIds);
+
+  // Iterates over the Id's returned to return book information
+  bookIds.forEach(bookId => {
+    fetchBookData(bookId)
+    .then(bookData => bookData.json())
+    .then(addBook)
+  });
+}
+
+function getBookIds() {
+  const findBooks = document.getElementsByClassName('book');
+  console.log(findBooks);
+  let values = [];
+  // Loop returns the book ID stored in the element
+  for (let item of findBooks) {
+    values.push(item.dataset.bookId);
+  }
+  return values;
+};
+
+function fetchBookData(bookId) {
+  return fetch('https://www.googleapis.com/books/v1/volumes/' + bookId)
+  // .then(data => {return data.json()})
+  // .then(res=>{console.log(res)})
+};
+
+function addBook(bookData) {
+  const el = document.querySelectorAll('*[data-book-id="' + bookData.id + '"]');
+  console.log(el);
+  el[0].outerHTML = '<div data-book-id="' + bookData.id + '" class="book hover"><a target="blank" href="' + bookData.volumeInfo.infoLink + '"><img src="' + bookData.volumeInfo.imageLinks.thumbnail + '" alt="' + bookData.volumeInfo.title + '"></a></div>';
+  console.log(bookData);
+};
