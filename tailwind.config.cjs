e8398a2149279ecac69b449abcf1e991cf3cce97
Ryan Harman <ryan.harman63@gmail.com>
2022-10-28 10:52:14 +0100

    Correction to actions config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
