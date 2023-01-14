41ece4112c247729c25369a1b857c78882c1e6d8
Ryan Harman <ryan.harman63@gmail.com>
2023-01-14 13:51:26 +0000

    Fix mobile

--- src/components/react/DotsRectangle.tsx
+++ src/components/react/DotsRectangle.tsx
@@ -4,8 +4,8 @@ export default function DotsRectangle() {
   const amount = new Array(540).fill(0);
   return (
     <div className="grid grid-cols-12 w-40 rotate-90">
-      {amount.map(() => (
-        <Dots />
+      {amount.map((_, idx) => (
+        <Dots key={idx} />
       ))}
     </div>
   );
