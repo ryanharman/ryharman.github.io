092b91dab9b6ac1d9c5a88c86bbea667ec00c095
Ryan Harman <ryan.harman63@gmail.com>
2024-12-12 15:06:31 +0000

    feat: rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,34 @@
-// import rss from "@astrojs/rss";
-// import { getCollection } from "astro:content";
-// import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-// export async function get(context) {
-//   const posts = await getCollection("thoughts");
-//   return rss({
-//     title: SITE_TITLE,
-//     description: SITE_DESCRIPTION,
-//     site: context.site,
-//     items: posts.map((post) => ({
-//       ...post.data,
-//       link: `/thoughts/${post.slug}/`,
-//     })),
-//   });
-// }
+export async function get(context) {
+  const posts = await getCollection("thoughts");
+  const content = await getCollection("content");
+  const experience = await getCollection("experience");
+
+  const posts_content_experience = [...posts, ...content, ...experience];
+
+  const collectionMap = {
+    experience: "/cv",
+    projects: "/",
+    thoughts: "/thoughts",
+  };
+
+  const rssItems = posts_content_experience.map((entry) => {
+    let link = collectionMap[entry.collection];
+
+    if (entry.collection === "thoughts") {
+      link = `/thoughts/${entry.slug}/`;
+    }
+
+    return { ...entry.data, link };
+  });
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: rssItems,
+  });
+}
