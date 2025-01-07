68e5500dd07a52e9f674b2a26675f3d5c1b7873c
Ryan Harman <ryan.harman63@gmail.com>
2025-01-07 18:43:11 +0000

    feat: export pdf cv and dark mode toggling prep

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-    keyframes: {
-      wiggle: {
-        "0%, 100%": { transform: "rotate(3deg)" },
-        "50%": { transform: "rotate(-3deg)" },
-      },
-    },
-    animation: {
-      wiggle: "wiggle 1s ease-in-out infinite",
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: ["selector", "class", '[data-theme="dark"]'],
+	theme: {
+		extend: {
+			colors: {
+				primary: "hsl(var(--primary))",
+				secondary: "hsl(var(--secondary))",
+				tertiary: "hsl(var(--tertiary))",
+				background: "hsl(var(--background))",
+			},
+		},
+		keyframes: {
+			wiggle: {
+				"0%, 100%": { transform: "rotate(3deg)" },
+				"50%": { transform: "rotate(-3deg)" },
+			},
+		},
+		animation: {
+			wiggle: "wiggle 1s ease-in-out infinite",
+		},
+	},
+	plugins: [],
 };
