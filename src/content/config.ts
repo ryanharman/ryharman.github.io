0a9388930fb3b7bfe0bec356ee030fb5c88ccd2b
Ryan Harman <ryan.harman63@gmail.com>
2025-01-11 13:46:28 +0000

    feat: thought thought thought

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,34 +1,35 @@
 import { defineCollection, z } from "astro:content";
 
 const thoughts = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    publishedAt: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-  }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		tags: z.array(z.string()),
+		publishedAt: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+	}),
 });
 
 const projects = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    link: z.string().url(),
-    stack: z.string(),
-  }),
+	schema: z.object({
+		title: z.string(),
+		link: z.string().url(),
+		stack: z.string(),
+	}),
 });
 
 const experience = defineCollection({
-  schema: z.object({
-    company: z.string(),
-    "company-link": z.string().url(),
-    title: z.string(),
-    stack: z.string(),
-    location: z.string(),
-    "start-date": z.string().or(z.date()),
-    "end-date": z.string().or(z.date()),
-  }),
+	schema: z.object({
+		company: z.string(),
+		"company-link": z.string().url(),
+		title: z.string(),
+		stack: z.string(),
+		location: z.string(),
+		"start-date": z.string().or(z.date()),
+		"end-date": z.string().or(z.date()),
+	}),
 });
 
 export const collections = { thoughts, projects, experience };
