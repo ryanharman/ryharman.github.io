17819ed3c0966f40e14a29d793856bd17ee9a572
Ryan Harman <ryan.harman63@gmail.com>
2025-01-12 17:07:37 +0000

    feat: robot txt and sitemap in head

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ryanharman.dev",
+	prefetch: true,
 	integrations: [mdx(), sitemap(), tailwind()],
 });
