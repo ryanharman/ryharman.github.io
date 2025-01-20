84ab28343c5df213b0b7f36bbb3dd7b472751049
ryanharman <ryan.harman@chatloop.com>
2025-01-20 14:03:57 +0000

    feat: use cf pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,33 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+const {
+  values: { mode },
+} = parseArgs({
+  options: { mode: { type: "string", default: process.env.NODE_ENV } },
+  strict: false,
+});
+
+const { SITE_URL } = loadEnv(mode, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ryanharman.dev",
-	prefetch: true,
-	integrations: [mdx(), sitemap(), tailwind()],
+  adapter: cloudflare(),
+  env: {
+    schema: {
+      SITE_URL: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+        url: true,
+      }),
+    },
+  },
+  site: SITE_URL ?? "http://localhost:4321",
+  output: "server",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
