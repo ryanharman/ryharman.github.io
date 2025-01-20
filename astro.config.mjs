dec2d999487b20301e4ec5a429c680883c16e6f3
ryanharman <ryan.harman@chatloop.com>
2025-01-20 14:20:44 +0000

    fix: envfield

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import { parseArgs } from "node:util";
 import { loadEnv } from "vite";
 
